8f95f0efc7b0c52b01ea48b07716d9623cf5e46b
sndpw <sandeepkumartirumala@gmail.com>
2024-03-28 23:54:51 +0530

    checkpoint3.2 styling

--- apps/client/components/AuthForm.tsx
+++ apps/client/components/AuthForm.tsx
@@ -1,48 +1,80 @@
-'use client';
+"use client";
 
-import { Session } from 'next-auth';
-import { signIn, signOut } from 'next-auth/react';
-import { FormEvent, useState } from 'react';
+import { Session } from "next-auth";
+import { signIn, signOut } from "next-auth/react";
+import { FormEvent, useState } from "react";
+import { Button, Card, Typography, TextField, Divider} from "@mui/material";
+import GoogleIcon from '@mui/icons-material/Google';
+// import IconButton from "@mui/material/IconButton";
+// import Fingerprint from "@mui/icons-material/Fingerprint";
 
 interface Props {
   session: Session | null;
 }
 
 export default function AuthForm({ session }: Props) {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const handleEmailSignIn = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // prevent form submission from refreshing the page
-    await signIn('nodemailer', { email, callbackUrl: '/protected' });
+    await signIn("nodemailer", { email, callbackUrl: "/protected" });
   };
 
   const handleGoogleSignIn = async () => {
-    await signIn('google', { callbackUrl: '/protected' });
+    await signIn("google", { callbackUrl: "/protected" });
   };
 
   const handleSignOut = async () => {
-    await signOut({ callbackUrl: '/' });
+    await signOut({ callbackUrl: "/" });
   };
 
   return (
     <div>
       {!session && (
-        <>
-          <form onSubmit={handleEmailSignIn}>
-            <input
-              required
-              name="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <button>Continue</button>
-          </form>
-          <button onClick={handleGoogleSignIn}>Continue with Google</button>
-        </>
+        <div style={{ display: "flex", justifyContent: "center", paddingTop:30 }}>
+          <Card variant={"outlined"} style={{ width: 400, height:300, padding: 20}}>
+            <form onSubmit={handleEmailSignIn}>
+              <TextField style={{marginTop:35}}
+                required
+                fullWidth={true}
+                label="Email"
+                variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  paddingTop: "25px",
+                }}
+              >
+                <Button fullWidth={true} variant="contained">
+                  Continue
+                </Button>
+              </div>
+            </form>
+         <div style={{ paddingTop:25, paddingBottom:25 }}>
+            <Divider sx={{ borderBottomWidth: 5 }}>OR</Divider>
+         
+            </div>
+             
+            <div style={{ display: "flex", justifyContent: "center"}}>
+                
+              <Button onClick={handleGoogleSignIn} variant="contained"  fullWidth={true}>
+                <GoogleIcon style={{marginRight:5}}></GoogleIcon> Continue with Google
+             
+              </Button>
+            </div>
+          </Card>
+        </div>
       )}
 
-      {session && <button onClick={handleSignOut}>Sign out</button>}
+      {session && (
+        <Button onClick={handleSignOut} variant="contained">
+          Sign out
+        </Button>
+      )}
     </div>
   );
 }
